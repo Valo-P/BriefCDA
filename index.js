7f28c3a8fb1971047bd5b9e052f7814e4003867f
ValentinP0 <valentin.poigt@outlook.fr>
2023-04-12 10:59:04 +0200

    Add views template

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import express from "express";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", "src/views");
+
+app.use(express.static(path.join(__dirname,"public")))
 app.get('/', (req, res) => {
     res.send("Hello, world!");
 })
