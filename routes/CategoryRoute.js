3e7a6748410ab884852e417588f7a88266ef7d48
ValentinP0 <valentin.poigt@outlook.fr>
2023-04-13 13:12:29 +0200

    CRUD Category

--- routes/CategoryRoute.js
+++ routes/CategoryRoute.js
@@ -1,9 +1,11 @@
 import express from "express";
-import {addCategory, getCategories} from "../src/controllers/CategoryController.js";
+import {addCategory, getCategories, getCategory, updateCategory, deleteCategory} from "../src/controllers/CategoryController.js";
 
 const router = express.Router();
 
 router.get("/categories", getCategories);
+router.get("/category", getCategory);
 router.post("/create-category", addCategory);
-
+router.post("/update-category", updateCategory);
+router.post("/delete-category", deleteCategory);
 export default router;
--- src/controllers/CategoryController.js
+++ src/controllers/CategoryController.js
@@ -1,9 +1,29 @@
 import Category from "../models/Category.js";
-export const getCategories = (req,res,next) => {
-    res.render('category/getCategories', {
+export const getCategories = async (req,res,next) => {
+
+    const categories = await Category.find();
+    console.log("Categories find");
+    console.log(categories);
+    console.log(categories.length);
+
+    // res.status(200).json({ categories });
+    res.status(200).render('category/getCategories', {
         title: "CategoryList",
+        categories: categories,
     });
 };
+
+export const getCategory = async (req, res, next) => {
+    const categoryID = req.body.categoryID;
+
+    const category = await Category.findById({
+        _id: categoryID,
+    });
+
+    console.log("Category find");
+    console.log(category);
+    res.status(200).json({ category });
+}
 
 export const addCategory = async (req, res, next) => {
     const categoryName = req.body.categoryName;
@@ -12,7 +32,36 @@ export const addCategory = async (req, res, next) => {
         categoryName,
     });
 
+    console.log("Category created");
     console.log(category);
     res.status(201).json({ category });
     // res.status(201).redirect("/categories");
 };
+
+export const updateCategory = async (req,res,next) => {
+    const categoryID = req.body.categoryID;
+    const categoryName = req.body.categoryName;
+
+    const category = await Category.findByIdAndUpdate({
+        _id: categoryID,
+    },{
+        categoryName,
+    },{
+        new: true,
+    })
+
+    console.log("Category updated");
+    console.log(category);
+    res.status(200).json({ category });
+}
+
+export const deleteCategory = async (req, res, next) => {
+    const categoryID = req.body.categoryID;
+
+    const category = await Category.findByIdAndDelete({
+        _id: categoryID,
+    })
+
+    console.log("Category deleted")
+    res.status(200).json({ Message: "Category deleted"});
+}
