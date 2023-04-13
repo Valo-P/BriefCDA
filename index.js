ff5e3d944f14f175dc468f4c14fe2c905eba3e78
ValentinP0 <valentin.poigt@outlook.fr>
2023-04-13 15:17:19 +0200

    Ajout de Layouts / Navbar

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ app.use(bodyParser.json());
 
 app.set("view engine", "ejs");
 app.set("views", "src/views");
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
 app.use(express.static(path.join(__dirname,"public")))
 
